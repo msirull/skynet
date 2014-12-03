2b346791333ad318d2d5c93d55d83e5ee1e1cb38
Martin <martin@sirull.com>
2014-12-03 00:54:18 -0800

    more fixes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 pwd=`pwd`
 mkdir /etc/config
+find /etc/config -name "*.sh" -type f -exec chmod 775 {} \
+find /etc/skynet -name "*.sh" -type f -exec chmod 775 {} \
 $pwd/scripts/config_dl.sh /etc/config
 cp $pwd/generic_config/nginx.conf /etc/nginx/nginx.conf
 cp $pwd/generic_config/sshconfig /root/.ssh/config
