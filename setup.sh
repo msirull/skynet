4c153fe1f86c17f2bf2689e9a63a416c5923c4b1
msirull <0101Proove71*>
2014-12-02 18:31:51 -0800

    fixes

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ mkdir /etc/config
 cp ./generic_config/nginx.conf /etc/nginx/nginx.conf
 cp ./generic_config/sshconfig /root/.ssh/config
 ./scripts/gitclone.sh
-python ./dynamo_config.py
+python ./scripts/service_registration.py
 cp ./generic_config/supervisord.conf /etc/
 cp ./scripts/skynet.py /etc/config/skynet_main.py
