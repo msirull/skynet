d415eb6eb72680e37d95e2c52d07da529274f173
Martin <martin@sirull.com>
2014-12-03 00:35:20 -0800

    path fixes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
+pwd=`pwd`
 mkdir /etc/config
-./scripts/config_dl.sh /etc/config
-cp ./generic_config/nginx.conf /etc/nginx/nginx.conf
-cp ./generic_config/sshconfig /root/.ssh/config
-./scripts/gitclone.sh
-python ./scripts/service_registration.py
-cp ./generic_config/supervisord.conf /etc/
-cp ./scripts/skynet.py /etc/config/skynet_main.py
+$pwd/scripts/config_dl.sh /etc/config
+cp $pwd/generic_config/nginx.conf /etc/nginx/nginx.conf
+cp $pwd/generic_config/sshconfig /root/.ssh/config
+$pwd/scripts/gitclone.sh
+python $pwd/scripts/service_registration.py
+cp $pwd/generic_config/supervisord.conf /etc/
+cp $pwd/scripts/skynet.py /etc/config/skynet_main.py
