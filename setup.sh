321bdf41331146a0f0f477d0f79f4da2c1ad5647
Martin <martin@sirull.com>
2014-12-03 17:38:30 -0800

    Making progress...

--- setup.sh
+++ setup.sh
@@ -11,4 +11,5 @@ python ./scripts/rp_config.py
 cp ./generic_config/supervisord.conf /etc/
 cp ./generic_config/supervisord /etc/rc.d/init.d
 chmod +x /etc/rc.d/init.d/supervisord
+cp ./generic_config/skynet.supervisor /etc/config
 cp ./scripts/skynet.py /etc/config/skynet_main.py
