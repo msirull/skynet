727db78da65145095ffc1c0d4614ea436bb92d1c
Martin <martin@sirull.com>
2014-12-04 02:28:53 -0800

    Updated listening ports

--- cloudformation templates/update_infrastructure.sh
+++ cloudformation templates/update_infrastructure.sh
@@ -1,14 +1,14 @@
-subdomain="testeast2"
+subdomain="testwest1"
 zoneapex="cloudnineit.net"
 configbucket="test-cf-templates"
-keypair="easttest"
+keypair="None"
 privaterepo="test"
 publicrepo="edge"
 branch="master"
 env="prod"
-stack_name="testeast-2"
-aregion="us-east-1"
+stack_name="TestWest"
+aregion="us-west-2"
 version=$(echo "0.1")
 mq=$(echo "$repo-$env-maint")
 aws s3 cp /Users/Me/Repos/skynet/cloudformation\ templates/php-nginx.json s3://$configbucket --profile test
-aws cloudformation create-stack --profile test --region "$aregion" --stack-name "$stack_name" --capabilities "CAPABILITY_IAM" --template-url https://s3.amazonaws.com/"$configbucket"/php-nginx.json --parameters ParameterKey=PublicRepo,ParameterValue="$publicrepo",UsePreviousValue=false ParameterKey=PrivateRepo,ParameterValue="$privaterepo",UsePreviousValue=false ParameterKey=KeyPair,ParameterValue="$keypair",UsePreviousValue=false ParameterKey=ZoneApex,ParameterValue="$zoneapex",UsePreviousValue=false ParameterKey=SubDomain,ParameterValue="$subdomain",UsePreviousValue=false ParameterKey=Branch,ParameterValue="$branch",UsePreviousValue=false ParameterKey=Environment,ParameterValue="$env",UsePreviousValue=false
+aws cloudformation update-stack --profile test --region "$aregion" --stack-name "$stack_name" --capabilities "CAPABILITY_IAM" --template-url https://s3.amazonaws.com/"$configbucket"/php-nginx.json --parameters ParameterKey=PublicRepo,ParameterValue="$publicrepo",UsePreviousValue=false ParameterKey=PrivateRepo,ParameterValue="$privaterepo",UsePreviousValue=false ParameterKey=KeyPair,ParameterValue="$keypair",UsePreviousValue=false ParameterKey=ZoneApex,ParameterValue="$zoneapex",UsePreviousValue=false ParameterKey=SubDomain,ParameterValue="$subdomain",UsePreviousValue=false ParameterKey=Branch,ParameterValue="$branch",UsePreviousValue=false ParameterKey=Environment,ParameterValue="$env",UsePreviousValue=false
