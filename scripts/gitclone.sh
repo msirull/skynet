b0ff9b187f14d15a4d0a27dff8bbe7e2bddc8ff2
Martin <martin@sirull.com>
2014-12-04 00:44:48 -0800

    Fixed some parallelism problems

--- scripts/gitclone.sh
+++ scripts/gitclone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 iid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-jtags=`aws ec2 describe-tags --region us-west-2 --filter "Name=resource-id,Values=$iid"`
+region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed s'/.$//')
+jtags=`aws ec2 describe-tags --region $region --filter "Name=resource-id,Values=$iid"`
 ltags=${jtags,,}
 tags=$(echo $ltags | jq '.tags | from_entries')
 branch=`echo $tags | jq -r '.branch'`
@@ -9,5 +10,4 @@ dir="/etc/app/"
 git config —global user.name "$iid"
 git config —global user.email "support@cloudnineit.com"
 git clone git@github.com:msirull/$repo.git $dir
-#git pull git@github.com:msirull/$repo.git $dir
 cp $dir* /var/www/html/
