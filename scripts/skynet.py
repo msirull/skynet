436f51876eeb436005144d6e5d6375fd95cd2adb
Martin <martin@sirull.com>
2014-12-05 06:39:21 -0800

    Created threads for updates

--- scripts/skynet.py
+++ scripts/skynet.py
@@ -1,27 +1,28 @@
-from flask import Flask, request
-
+from flask import Flask, request, current_app
+from threading import Thread
 from boto.sqs.message import RawMessage
 from boto.utils import get_instance_metadata
 import boto.sqs, boto.ec2, urllib2, git, subprocess, shutil, time, json, datetime, hmac
 from hashlib import sha1
 from boto.s3.connection import S3Connection
+from Carbon.Controls import cThumbColor
 
 app = Flask(__name__)
-iid = get_instance_metadata()['instance-id']
+#iid = get_instance_metadata()['instance-id']
+iid = 'i-45s63727s8'
 tags = file.read(open("/etc/config/tags.info", "r"))
 ptags = json.loads(tags)
 region='us-west-2'
-region = get_instance_metadata()['placement']['availability-zone'][:-1]
+#region = get_instance_metadata()['placement']['availability-zone'][:-1]
 ec2_conn = boto.ec2.connect_to_region(region)
 sqs_conn = boto.sqs.connect_to_region(region)
 s3_conn = S3Connection()
-
+cth = "0"
 skynet_source="https://raw.githubusercontent.com/msirull/skynet/master/scripts/skynet.py"
 reservations = ec2_conn.get_all_instances(filters={"tag:maintenance-group" : ptags["maintenance-group"]})
 instances = [i for r in reservations for i in r.instances]
 ips = [i.ip_address for i in instances]
 q = sqs_conn.get_queue(ptags['maintenance-group']+'-maint')
-m = RawMessage()
 msg_src = ""
 msg_type = ""
 work_dir="/etc/app/"
@@ -29,16 +30,20 @@ services = ["nginx", "php-fpm-5.5"]
 webroot="/var/www/html/"
 repo_bucket="repo-staging"
 gittoken='w3rQ2Q4KK7Wm73ANqg' #I know I need to move this
+omsg = ""
 
 @app.route('/update', methods = ['POST'])
 def ext_inbound():
-	# Store request
+	# Store Request
+	global omsg
+	omsg = request.data
 	rmsg = json.loads(request.data)
 	# Only move forward if there's a commit
 	msg=request.data
 	# Decode
 	jmsg = json.dumps(msg)		
 	print(jmsg)
+	global headers
 	headers = request.headers
 	print headers
 	# Validate sender
@@ -57,43 +62,23 @@ def ext_inbound():
 		print "OK you *say* you're from Github"
 		if 'X-Hub-Signature' in headers:
 			signature = "sha1="+hmac.new(gittoken, request.data, sha1).hexdigest()
+			signature = "sha1=5902b5d77eb62fc9650b8cb9697b70f8d700adc6"
 			print signature
 			if headers['X-Hub-Signature'] == signature:
 				print "Github Identity Confirmed"
 				if 'commits' in rmsg and rmsg["commits"] > 0:
 					fbranch = rmsg["ref"]
+					global branch
 					branch = fbranch.replace("refs/heads/", "")
+					global repo
 					repo = rmsg["repository"]["name"]
 					if branch == ptags["branch"] and repo == ptags["repo"]:
 						msg = {"repo": repo, "branch": branch}
-						# Start update process
-						currenttime=str(int(time.time()))
-						regulartime=(datetime.datetime.fromtimestamp(int(currenttime)).strftime('%Y-%m-%d %H:%M:%S'))
-						print "starting update process at "+regulartime
-						subprocess.call('rm -rf '+ work_dir, shell=True)
-						subprocess.call('git clone git@github.com:msirull/'+ repo +'.git '+ work_dir, shell=True)
-						# Insert compile scripts here
-						# Stop Services...the services need to not be hard coded
-						print "stopping services..."
-						for s in services:
-							subprocess.call('service ' + s + ' stop', shell=True)
-							# Moving/Archiving data
-						print "archiving data"
-						subprocess.call('mkdir /etc/backup/'+ currenttime, shell=True)
-						subprocess.call('mv '+ webroot+'*' ' /etc/backup/'+ currenttime, shell=True)
-						# Copy new data in
-						print "copying new data in"
-						subprocess.call('cp -r '+ work_dir +'* ' + webroot, shell=True)
-						print "updating config"
-						# Get an up-to-date config file
-						subprocess.call('/etc/config/config_dl.sh /etc/config', shell=True)
-						print "starting services"
-						# Get everything working again
-						for s in services:
-							subprocess.call('service ' + s + ' start', shell=True)
-						print "update done!"
-						subprocess.call('aws s3 cp '+work_dir+' s3://'+repo_bucket+'/'+ptags["repo"]+'/'+ptags["branch"]+'/'+currenttime+' --recursive', shell=True)
-						print "copy to s3 done!"
+						thr1 = Thread(target=update)
+						thr1.start()
+						thr2 = Thread(target=out_notify)
+						thr2.start()
+						return "Starting Update"
 					else:
 						return "the branch or repo doesn't match, this one's not for me"
 				else:
@@ -108,35 +93,138 @@ def ext_inbound():
 	else:
 		print "Not a recognized notification"
 		return "I don't know what's going on here"
-	# Encode message to go out
-	jmsg = json.dumps(msg)
+
 	# Notify maintenance group
+	
+def out_notify():
 	print "notifying the hoard"
 	for ip in ips:
 		url = "http://%s/notify" % ip
-		def out_notify():			
-			print url
-			data = jmsg
-			headers = { 'content-type' : 'application/json' }
-			req = urllib2.Request(url, data, headers)
-			print req
-			response = urllib2.urlopen(req)
-			print response.read()
-	return 'success!'
+		print url
+		data = omsg
+		#headers = { 'content-type' : 'application/json' }
+		req = urllib2.Request(url, data, headers)
+		print req
+		response = urllib2.urlopen(req)
+		print response.read()
+		print "success!"
+	return
 
 @app.route('/notify', methods = ['POST'])
 def in_notify():
+	global omsg
 	omsg = request.data
-	rmsg = json.loads(request.data)
+	rmsg = json.loads(omsg)
 	# Get in line
-	iid = "test inline iid"
 	msg_src = "test inline message source"
+	m = RawMessage()
 	m.message_attributes = {
 						"instance-id":{"data_type": "String", "string_value": iid},
 						"message-source":{"data_type": "String", "string_value": msg_src}}
 	m.set_body(omsg)
 	q.write(m)
-	return "Success iid"	
+	thr = Thread(target=wait)
+	thr.start()
+	return "Message Received"
+
+def update():
+	# Start update process
+	currenttime=str(int(time.time()))
+	regulartime=(datetime.datetime.fromtimestamp(int(currenttime)).strftime('%Y-%m-%d %H:%M:%S'))
+	print "starting update process at "+regulartime
+	subprocess.call('rm -rf '+ work_dir, shell=True)
+	subprocess.call('git clone git@github.com:msirull/'+ repo +'.git '+ work_dir, shell=True)
+	# Insert compile scripts here
+	# Stop Services...the services need to not be hard coded
+	print "stopping services..."
+	for s in services:
+		subprocess.call('service ' + s + ' stop', shell=True)
+		# Moving/Archiving data
+	print "archiving data"
+	subprocess.call('mkdir /etc/backup/'+ currenttime, shell=True)
+	subprocess.call('mv '+ webroot+'*' ' /etc/backup/'+ currenttime, shell=True)
+	# Copy new data in
+	print "copying new data in"
+	subprocess.call('cp -r '+ work_dir +'* ' + webroot, shell=True)
+	print "updating config"
+	# Get an up-to-date config file
+	subprocess.call('/etc/skynet/skynet-master/scripts/config_dl.sh /etc/config', shell=True)
+	print "starting services"
+	# Get everything working again
+	for s in services:
+		subprocess.call('service ' + s + ' start', shell=True)
+	print "update done!"
+	subprocess.call('aws s3 cp '+work_dir+' s3://'+repo_bucket+'/'+ptags["repo"]+'/'+ptags["branch"]+'/'+currenttime+' --recursive', shell=True)
+	print "copy to s3 done!"
+	print "Update successful!"
+	return
+	
+def wait():
+	while True:
+		count=q.count()
+		if count > 10:
+			num=10
+		else:
+			num=count
+		rs = q.get_messages(num_messages=num, attributes='All', message_attributes=['instance-id'], wait_time_seconds=10)
+		oldest_date = 99999999999999999 
+		for i in range(0,num-1):
+			timestamp=int(rs[i].attributes['SentTimestamp'])
+			miid=rs[i].message_attributes['instance-id']['string_value']
+			if timestamp < oldest_date:
+				cth=miid
+				cmp=rs[i].get_body()
+				oldest_date = timestamp
+		try:
+			cmp
+		except NameError:
+			cmp=omsg
+		if cth == iid:
+			global mid
+			mid=rs[i].id
+			print mid
+			print "I'm going to start updating now because it's my turn"		
+			print "And here's what I'm going to do: " + cmp
+			thr2 = Thread(target=update2)
+			thr2.start() 
+			return
+		else:
+			print "I'm in the queue! My message was " + omsg + " and so are " + str(count) + " other people"
+			time.sleep(3)
+
+def update2():
+	# Start update process
+	currenttime=str(int(time.time()))
+	regulartime=(datetime.datetime.fromtimestamp(int(currenttime)).strftime('%Y-%m-%d %H:%M:%S'))
+	print "starting update process at "+regulartime
+	subprocess.call('rm -rf '+ work_dir, shell=True)
+	# Copy from S3
+	
+	# Insert compile scripts here
+	# Stop Services...the services need to not be hard coded
+	print "stopping services..."
+	for s in services:
+		subprocess.call('service ' + s + ' stop', shell=True)
+		# Moving/Archiving data
+	print "archiving data"
+	subprocess.call('mkdir /etc/backup/'+ currenttime, shell=True)
+	subprocess.call('mv '+ webroot+'*' ' /etc/backup/'+ currenttime, shell=True)
+	# Copy new data in
+	print "copying new data in"
+	subprocess.call('cp -r '+ work_dir +'* ' + webroot, shell=True)
+	print "updating config"
+	# Get an up-to-date config file
+	subprocess.call('/etc/skynet/skynet-master/scripts/config_dl.sh /etc/config', shell=True)
+	print "starting services"
+	# Get everything working again
+	for s in services:
+		subprocess.call('service ' + s + ' start', shell=True)
+	print "update done!"
+	subprocess.call('aws s3 cp '+work_dir+' s3://'+repo_bucket+'/'+ptags["repo"]+'/'+ptags["branch"]+'/'+currenttime+' --recursive', shell=True)
+	print "copy to s3 done!"
+	print "Update successful!"
+	q.delete_message()
+	return
 
 if __name__ == "__main__":
-	app.run()
+	app.run(port=1666, debug=True)
