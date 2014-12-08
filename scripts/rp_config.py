d00924ac964eb18bd249b90437f37de1fb113579
Martin <martin@sirull.com>
2014-12-08 00:47:52 -0800

    Web hook Private Subnet Update
    Updates to make sure web hooks work behind a firewall

--- scripts/rp_config.py
+++ scripts/rp_config.py
@@ -22,3 +22,22 @@ for items in stack_items:
 	file.write("}\n")
 	file.close()
 subprocess.call('service nginx reload', shell=True)
+
+if os.path.exists("/etc/config/rpc.locations"):
+	os.remove("/etc/config/rpc.locations")
+env = ptags['environment']
+tbl = "endpoints"
+locations = {}
+table_obj = Table(tbl)
+stack_items = table_obj.query_2(env__eq=env)
+for items in stack_items:
+	stack=items['layer']+"/"+items['env']
+	url=items['url']
+	locations[stack] = url
+	file = open("/etc/config/rpc.locations", "w+")
+	file.write("location /" + stack +"/ {\n")
+	file.write("proxy_pass http://"+ locations[stack] +":1666;"+"\n")
+	file.write("proxy_redirect http://"+ locations[stack] +"/ /"+stack+"/;\n")
+	file.write("}\n")
+	file.close()
+subprocess.call('service nginx reload', shell=True)
