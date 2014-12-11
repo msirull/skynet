3970a0a6f9850d959633c73d74158032c5a43af8
Martin <martin@sirull.com>
2014-12-11 05:13:47 -0800

    Sumo Collector Setup

--- scripts/service_registration.py
+++ scripts/service_registration.py
@@ -1,6 +1,8 @@
 from boto.dynamodb2.table import Table
 import json, os, subprocess
 from boto.utils import get_instance_metadata
+
+# Register with DynamoDB
 tags = file.read(open("/etc/config/tags.info", "r"))
 ptags = json.loads(tags)
 env = ptags['environment']
--- scripts/sumo_config.py
+++ scripts/sumo_config.py
@@ -1,6 +1,27 @@
-import json
+import json, os, subprocess
+from boto.s3.connection import S3Connection
+from boto.utils import get_instance_metadata
+from boto.cloudformation import CloudFormationConnection
+iid = get_instance_metadata()['instance-id']
+cf_conn=CloudFormationConnection()
+tags = file.read(open("/etc/config/tags.info", "r"))
+ptags = json.loads(tags)
 
+config_bucket_obj=cf_conn.describe_stack_resource(ptags['aws:cloudformation:stack-name'],'s3globalconfig') 
+config_bucket=config_bucket_obj['DescribeStackResourceResponse']['DescribeStackResourceResult']['StackResourceDetail']['PhysicalResourceId']
+name = ptags['layer'] + ptags['environment'] + iid
 
+# Sumo Configuration
+s3_conn = S3Connection()
+cb_obj = s3_conn.get_bucket(config_bucket)
+sumoconfk = cb_obj.new_key('sumo.conf')
+sumoconf = sumoconfk.get_contents_as_string()
+sumoconf.replace("name=name", "name="+name)
+if os.path.exists("/etc/config/sumo.conf"):
+    os.remove("/etc/config/sumo.conf")
+file = open("/etc/config/sumo.conf", "a")
+file.write(sumoconf)
+file.close()
 
 # Sources file
 sourceconfig = {}
@@ -19,6 +40,9 @@ sourceconfig["sources"][1]["name"] = "CloudFormation Logs"
 sourceconfig["sources"][1]["pathExpression"] = "/var/log/cfn-init.log"
 sourceconfig["sources"][1]["category"] = "deployment"
 print sourceconfig
+if os.path.exists("/etc/config/default.sumo"):
+    os.remove("/etc/config/default.sumo")
 file = open("/etc/config/default.sumo", "a")
 file.write(json.dumps(sourceconfig))
 file.close()
+subprocess.call("/etc/init.d/collector start", shell=True)
